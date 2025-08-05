6f1a333a295469e5cae8b3498cc092cfd1c00d29
Satyam sharma <sharmasam93500@gmail.com>
2025-08-05 21:32:15 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import flowbiteReact from "flowbite-react/plugin/vite";
+import flowbiteReact from 'flowbite-react/plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +11,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    historyApiFallback: true,
   },
 });
